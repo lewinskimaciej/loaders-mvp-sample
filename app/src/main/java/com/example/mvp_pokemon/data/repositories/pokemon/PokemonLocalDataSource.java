4c154e602b10ceea1b14be127a9d273bc0709d76
lewinskimaciej <macieklewinski@gmail.com>
2017-01-24 15:14:14 +0100

    Fix inserting Pokemon into DB by inserting Sprites first.

--- app/src/main/java/com/example/mvp_pokemon/data/repositories/pokemon/PokemonLocalDataSource.java
+++ app/src/main/java/com/example/mvp_pokemon/data/repositories/pokemon/PokemonLocalDataSource.java
@@ -1,6 +1,7 @@
 package com.example.mvp_pokemon.data.repositories.pokemon;
 
 import com.example.mvp_pokemon.data.models.PokemonModel;
+import com.example.mvp_pokemon.data.models.SpritesModel;
 import com.example.mvp_pokemon.data.repositories.pokemon.interfaces.PokemonDataSource;
 
 import java.util.List;
@@ -32,20 +33,34 @@ public final class PokemonLocalDataSource implements PokemonDataSource {
 
     @Override
     public void savePokemon(final PokemonModel pokemonModel) {
-        database.insert(pokemonModel)
+        database.insert(pokemonModel.getSprites())
                 .subscribeOn(Schedulers.computation())
                 .observeOn(Schedulers.computation())
-                .subscribe(new DisposableSingleObserver<PokemonModel>() {
+                .subscribe(new DisposableSingleObserver<SpritesModel>() {
                     @Override
-                    public void onSuccess(PokemonModel value) {
-                        Timber.d("inserted");
+                    public void onSuccess(SpritesModel value) {
+                        database.insert(pokemonModel)
+                                .subscribeOn(Schedulers.computation())
+                                .observeOn(Schedulers.computation())
+                                .subscribe(new DisposableSingleObserver<PokemonModel>() {
+                                    @Override
+                                    public void onSuccess(PokemonModel value) {
+                                        Timber.d("inserted");
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+                                        Timber.d(e, "not inserted");
+                                    }
+                                });
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Timber.d(e, "not inserted");
+                        Timber.d("onError");
                     }
                 });
+
 
     }
 
