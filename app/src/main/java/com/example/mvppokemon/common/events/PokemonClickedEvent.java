b676067adba394d4c4a9419e9d413eacbf5fc140
Maciej Lewinski <maciej.lewinski@zaven.co>
2017-03-08 10:59:50 +0100

    Move events package to common

---
+++ app/src/main/java/com/example/mvppokemon/common/events/PokemonClickedEvent.java
@@ -0,0 +1,20 @@
+package com.example.mvppokemon.common.events;
+
+import com.example.mvppokemon.data.models.PokemonModel;
+
+
+public class PokemonClickedEvent {
+    private PokemonModel pokemonModel;
+
+    public PokemonClickedEvent(PokemonModel pokemonModel) {
+        this.pokemonModel = pokemonModel;
+    }
+
+    public PokemonModel getPokemonModel() {
+        return pokemonModel;
+    }
+
+    public void setPokemonModel(PokemonModel pokemonModel) {
+        this.pokemonModel = pokemonModel;
+    }
+}
